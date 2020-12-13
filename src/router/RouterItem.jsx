22c8a053f7b9d5df68825c43892fa48847b5116a
Rezady <rezady30@gmail.com>
2020-12-13 09:28:32 +0700

    fix package.json

--- src/router/RouterItem.jsx
+++ src/router/RouterItem.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import NavbarItem from "../components/homepage/NavbarItem.jsx";
 import MainItem from "../components/homepage/MainItem.jsx";
